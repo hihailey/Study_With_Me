b6343e244c9c981a514a2468e63b2f0d5f032d01
Megan Kuo <megan.kuo@live.com>
2022-03-31 21:14:11 -0700

    backend

--- resources/views/video_chat/index.blade.php
+++ resources/views/video_chat/index.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 @section('content')
+    {{ $user }}
+    <div>
+        <h1>Users in room</h1>
+        {{ $usersInRoom }}
+    </div>
+
 <video-chat :user="{{ $user }}" :others="{{ $others }}" :usersInRoom="{{ $usersInRoom}}" pusher-key="{{ config('broadcasting.connections.pusher.key') }}" pusher-cluster="{{ config('broadcasting.connections.pusher.options.cluster') }}"></video-chat>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,20 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::group(['middleware' => 'auth'], function(){
   Route::get('/video_chat', 'App\Http\Controllers\VideoChatController@index');
   Route::post('/auth/video_chat', 'App\Http\Controllers\VideoChatController@auth');
+
+    Route::get('/streaming', 'App\Http\Controllers\WebrtcStreamingController@index');
+    Route::get('/streaming/{streamId}', 'App\Http\Controllers\WebrtcStreamingController@consumer');
+    Route::post('/stream-offer', 'App\Http\Controllers\WebrtcStreamingController@makeStreamOffer');
+    Route::post('/stream-answer', 'App\Http\Controllers\WebrtcStreamingController@makeStreamAnswer');
+
+    // Dynamic Presence Channel for Streaming
+    Broadcast::channel('streaming-channel.{streamId}', function ($user) {
+        return ['id' => $user->id, 'name' => $user->name];
+    });
+
+// Signaling Offer and Answer Channels
+    Broadcast::channel('stream-signal-channel.{userId}', function ($user, $userId) {
+        return (int) $user->id === (int) $userId;
+    });
 });
 
