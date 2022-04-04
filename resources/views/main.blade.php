26dea3d480a6218969efc7bcb4536d3ab93a76e9
Megan Kuo <megan.kuo@live.com>
2022-04-03 20:52:10 -0700

    can display users

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,11 +1,23 @@
 @extends('layouts.app')
 @section('content')
 <div class="container-fluid">
-      @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-           <p>{{ $message }}</p>
-        </div>
-      @endif
+  @if (!Auth::guest())
+  @foreach($users as $user)
+  <ul>
+    <li class="user liUser" id="{{ $user->id }}">
+      <div class="media-body">
+
+        <a class='liA_user' href="/chats/{{ $user->id }}">{{ $user->name }}</a>
+      </div>
+    </li>
+  </ul>
+  @endforeach
+  @endif
+  @if ($message = Session::get('success'))
+  <div class="alert alert-success">
+    <p>{{ $message }}</p>
+  </div>
+  @endif
 </div>
 
 @endsection
